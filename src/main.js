4318f5b9952cc70122287bc6a45d1e9f067f0358
liyangyang <335044569@qq.com>
2019-06-28 11:37:20 +0800

    1. 项目基本结构搭建完成 2. 登录功能实现了一半 3. 我要开始招聘程序员了

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false;
+// 1. 引入elementUI库
+import ElementUI from 'element-ui'
+
+// 2. 引入css文件
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 3. 引入index页面css文件
+import './assets/css/index.css'
+
+// 3. 安装elementUi
+Vue.use(ElementUI)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-row type="flex" justify="center" align="middle" class="row-bg">
+    <el-col :xs="14" :sm="12" :md="10" :lg="8" :xl="6">
+      <el-form ref="form" :model="form" label-position="top" label-width="80px" class="login-from">
+        <el-form-item label="用户名">
+          <el-input v-model="form.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="form.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">登录</el-button>
+          <el-button>重置</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
+  </el-row>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    }
+  }
+};
+</script>
+
+<style>
+.row-bg {
+  height: 100%;
+}
+.login-from {
+  background-color: #fff;
+  padding: 30px 20px;
+  border-radius: 10px;
+  min-width: 400px;
+}
+</style>
